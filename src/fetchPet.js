486e4d5ba01bd8e2461c6904a4e5a10b5e36f2db
Harun Sahinol <mehmethsh@gmail.com>
2024-06-13 09:57:46 +0300

    react-query added

---
+++ src/fetchPet.js
@@ -0,0 +1,13 @@
+const fetchPet = async({queryKey})=>{
+  const id = queryKey[1];
+  
+  const resApi = await fetch(`https://pets-v2.dev-apis.com/pets?id=${id}`);
+
+  if(!resApi.ok){
+    throw new Error("Something went wrong");
+  }
+
+  return resApi.json();
+}
+
+export default fetchPet;
